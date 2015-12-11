e5ada96f0b485c6e141b4063aae060cf1097a5b3
Siceron <ludovicfastre@yahoo.fr>
2015-12-11 16:45:49 +0100

    Convolution from server

--- src/server/Client.java
+++ src/server/Client.java
@@ -1,8 +1,6 @@
 package server;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.ConvolveOp;
-import java.awt.image.Kernel;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -19,6 +17,14 @@ public class Client {
 		// Client
 	}
 
+	private static int[] stringToIntArray(String stringArray[]) {
+		int result[] = new int[stringArray.length];
+		for(int i = 0 ; i<stringArray.length ; i++){
+			result[i] = Integer.parseInt(stringArray[i]);
+		}
+		return result;
+	}
+	
 	private static int[][] imageTo2DArray(BufferedImage image) {
 
 		int w = image.getWidth();
@@ -33,11 +39,11 @@ public class Client {
 	}
 
 	private static void writeImage(int arr[][]){
-		int xLenght = arr.length;
+		int xLength = arr.length;
 		int yLength = arr[0].length;
-		BufferedImage b = new BufferedImage(xLenght, yLength, BufferedImage.TYPE_INT_ARGB);
+		BufferedImage b = new BufferedImage(xLength, yLength, BufferedImage.TYPE_INT_ARGB);
 
-		for(int x = 0; x < xLenght; x++) {
+		for(int x = 0; x < xLength; x++) {
 			for(int y = 0; y < yLength; y++) {
 				b.setRGB(x, y, arr[x][y]);
 			}
@@ -49,37 +55,14 @@ public class Client {
 		}
 	}
 
-	static final float[] blur = {
-		0.111f, 0.111f, 0.111f, 
-		0.111f, 0.111f, 0.111f, 
-		0.111f, 0.111f, 0.111f, 
-	};
-
-	static final float[] sharpen = {
-		0.0f, -1.0f, 0.0f,
-		-1.0f, 5.0f, -1.0f,
-		0.0f, -1.0f, 0.0f
-	};
-
-	static final float[] edges = {
-		-1.0f, -1.0f, -1.0f,
-		-1.0f, 8.0f, -1.0f,
-		-1.0f, -1.0f, -1.0f
-	};
-
 	public static void main(String [] args)
 	{
 		String serverName = args[0];
 		int port = Integer.parseInt(args[1]);
 		try
 		{
-			BufferedImage hugeImage = ImageIO.read(Client.class.getResource("image.png"));
-
-			Kernel kernel = new Kernel(3, 3, edges);
-			ConvolveOp op = new ConvolveOp(kernel);
-			BufferedImage dstImage = op.filter(hugeImage, null);
-			int[][] result = imageTo2DArray(dstImage);
-			writeImage(result);
+			BufferedImage image = ImageIO.read(Client.class.getResource("image.png"));
+			int[][] result = imageTo2DArray(image);
 
 			System.out.println("Connecting to " + serverName +
 					" on port " + port);
@@ -88,12 +71,35 @@ public class Client {
 					+ client.getRemoteSocketAddress());
 			OutputStream outToServer = client.getOutputStream();
 			DataOutputStream out = new DataOutputStream(outToServer);
-			out.writeUTF("Hello from "
-					+ client.getLocalSocketAddress());
+			
+			// SENDING IMAGE TO SERVER
+			String messageToServer = "";
+			out.writeUTF(""+result.length);
+			out.writeUTF(""+result[0].length);
+			for(int i = 0 ; i < result.length ; i++){
+				for(int j = 0 ; j < result[0].length ; j++){
+					if(j < result[0].length-1)
+						messageToServer += result[i][j]+",";
+					else
+						messageToServer += result[i][j]+"";
+				}
+				out.writeUTF(messageToServer);
+				messageToServer = "";
+			}
+			//out.writeUTF("Hello from "
+			//		+ client.getLocalSocketAddress());
+			
+			// RECEIVING IMAGE FROM SERVER
 			InputStream inFromServer = client.getInputStream();
 			DataInputStream in =
 					new DataInputStream(inFromServer);
-			System.out.println("Server says " + in.readUTF());
+			int lengthX = Integer.parseInt(in.readUTF());
+			int lengthY = Integer.parseInt(in.readUTF());
+			int imageArray[][] = new int[lengthX][lengthY];
+			for(int i = 0 ; i<lengthX ; i++){
+				imageArray[i] = stringToIntArray(in.readUTF().split(","));
+			}
+			writeImage(imageArray);
 			client.close();
 		}catch(IOException e)
 		{
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,5 +1,8 @@
 package server;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.ConvolveOp;
+import java.awt.image.Kernel;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -8,54 +11,134 @@ import java.net.Socket;
 import java.net.SocketTimeoutException;
 
 public class Server extends Thread {
-	private ServerSocket serverSocket;
-	   
-	   public Server(int port) throws IOException
-	   {
-	      serverSocket = new ServerSocket(port);
-	      //serverSocket.setSoTimeout(60000);
-	   }
 
-	   public void run()
-	   {
-	      while(true)
-	      {
-	         try
-	         {
-	            System.out.println("Waiting for client on port " +
-	            serverSocket.getLocalPort() + "...");
-	            Socket server = serverSocket.accept();
-	            System.out.println("Just connected to "
-	                  + server.getRemoteSocketAddress());
-	            DataInputStream in =
-	                  new DataInputStream(server.getInputStream());
-	            System.out.println(in.readUTF());
-	            DataOutputStream out =
-	                 new DataOutputStream(server.getOutputStream());
-	            out.writeUTF("Thank you for connecting to "
-	              + server.getLocalSocketAddress() + "\nGoodbye!");
-	            server.close();
-	         }catch(SocketTimeoutException s)
-	         {
-	            System.out.println("Socket timed out!");
-	            break;
-	         }catch(IOException e)
-	         {
-	            e.printStackTrace();
-	            break;
-	         }
-	      }
-	   }
-	   public static void main(String [] args)
-	   {
-	      int port = Integer.parseInt(args[0]);
-	      try
-	      {
-	         Thread t = new Server(port);
-	         t.start();
-	      }catch(IOException e)
-	      {
-	         e.printStackTrace();
-	      }
-	   }
+	private ServerSocket serverSocket;
+	private final float[] edges = {
+			-1.0f, -1.0f, -1.0f,
+			-1.0f, 8.0f, -1.0f,
+			-1.0f, -1.0f, -1.0f
+	};
+	private final float[] sharpen = {
+			0.0f, -1.0f, 0.0f,
+			-1.0f, 5.0f, -1.0f,
+			0.0f, -1.0f, 0.0f
+	};
+	private final float[] blur = {
+			0.111f, 0.111f, 0.111f, 
+			0.111f, 0.111f, 0.111f, 
+			0.111f, 0.111f, 0.111f, 
+	};
+
+	public Server(int port) throws IOException
+	{
+		serverSocket = new ServerSocket(port);
+		//serverSocket.setSoTimeout(60000);
+	}
+
+	private int[] stringToIntArray(String stringArray[]) {
+		int result[] = new int[stringArray.length];
+		for(int i = 0 ; i<stringArray.length ; i++){
+			result[i] = Integer.parseInt(stringArray[i]);
+		}
+		return result;
+	}
+
+	private BufferedImage arrayToImage(int imageArray[][]){
+		int xLength = imageArray.length;
+		int yLength = imageArray[0].length;
+		BufferedImage b = new BufferedImage(xLength, yLength, BufferedImage.TYPE_INT_ARGB);
+
+		for(int x = 0; x < xLength; x++) {
+			for(int y = 0; y < yLength; y++) {
+				b.setRGB(x, y, imageArray[x][y]);
+			}
+		}
+
+		return b;
+	}
+
+	private int[][] imageTo2DArray(BufferedImage image) {
+
+		int w = image.getWidth();
+		int h = image.getHeight();
+		int[][] pixels = new int[w][h];
+
+		for( int i = 0; i < w; i++ )
+			for( int j = 0; j < h; j++ )
+				pixels[i][j] = image.getRGB( i, j );
+
+		return pixels;
+	}
+
+	public void run()
+	{
+		while(true)
+		{
+			try
+			{
+				System.out.println("Listening on port " +
+						serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+				System.out.println("Just connected to "
+						+ server.getRemoteSocketAddress());
+
+				// PARSING IMAGE
+				DataInputStream in =
+						new DataInputStream(server.getInputStream());
+				int lengthX = Integer.parseInt(in.readUTF());
+				int lengthY = Integer.parseInt(in.readUTF());
+				int imageArray[][] = new int[lengthX][lengthY];
+				for(int i = 0 ; i<lengthX ; i++){
+					imageArray[i] = stringToIntArray(in.readUTF().split(","));
+				}
+
+				// CONVOLUTION
+				BufferedImage image = arrayToImage(imageArray);
+				Kernel kernel = new Kernel(3, 3, sharpen);
+				ConvolveOp op = new ConvolveOp(kernel);
+				BufferedImage dstImage = op.filter(image, null);
+				int[][] result = imageTo2DArray(dstImage);
+
+				// SENDING RESULT
+				DataOutputStream out =
+						new DataOutputStream(server.getOutputStream());
+				String messageToClient = "";
+				out.writeUTF(""+result.length);
+				out.writeUTF(""+result[0].length);
+				for(int i = 0 ; i < result.length ; i++){
+					for(int j = 0 ; j < result[0].length ; j++){
+						if(j < result[0].length-1)
+							messageToClient += result[i][j]+",";
+						else
+							messageToClient += result[i][j]+"";
+					}
+					out.writeUTF(messageToClient);
+					messageToClient = "";
+				}
+				//out.writeUTF("Thank you for connecting to "
+				//		+ server.getLocalSocketAddress() + "\nGoodbye!");
+				server.close();
+			}catch(SocketTimeoutException s)
+			{
+				System.out.println("Socket timed out!");
+				break;
+			}catch(IOException e)
+			{
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+	public static void main(String [] args)
+	{
+		int port = Integer.parseInt(args[0]);
+		try
+		{
+			Thread t = new Server(port);
+			t.start();
+		}catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
